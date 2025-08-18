686c08b17cf601a6acc837c33d9fc6db1b5c1317
eSteamation <sdaark@gmail.com>
2025-08-18 16:43:57 +0300

    API tests done

--- src/test/java/ReportPortal/Tests/APITest.java
+++ src/test/java/ReportPortal/Tests/APITest.java
@@ -2,10 +2,12 @@ package ReportPortal.Tests;
 
 import ReportPortal.API.DashboardCreator;
 import ReportPortal.Hooks.ApiTestExtension;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static ReportPortal.Utilities.ConfigLoader.getProperty;
+import static io.restassured.RestAssured.given;
 
 @ExtendWith(ApiTestExtension.class)
 public class APITest {
@@ -17,6 +19,18 @@ public class APITest {
     private static final String API_KEY = getProperty("API_KEY");
     private static final String TEMPLATE = getProperty("templateName");
     private final DashboardCreator dashboardCreator = new DashboardCreator();
+
+    @Test
+    @Order(1)
+    void HealthCheckAPI() {
+        given()
+                .baseUri(getProperty("URL_API"))
+                .header("Authorization", "Bearer " + API_KEY)
+                .when()
+                .get("/api/v1/" + PROJECT + "/dashboard")
+                .then()
+                .statusCode(200);
+    }
 
     @Test
     void CreationTestPositive() {
